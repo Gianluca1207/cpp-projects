aba0b281879c9da6d2f29d2624a94c3d4eb52502
Gianluca1207 <110056612+Gianluca1207@users.noreply.github.com>
2024-03-18 17:31:49 +0100

    student management system -1 commit

---
+++ student grades management system/classes.hpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Student {
+    std::string name;
+    int ID;
+    float grades[3];     //array to store the grades for 3 subjects
+
+    public:
+        Student (std::string name, int ID, float grades[]);     //this is a declaration of constructor (not a definition)
+
+        // setter methods
+        void setName (std::string newName) {
+            this->name = newName;
+        }
+
+        void setID (int newID) {
+            this->ID = newID;
+        }
+
+        //getter methods
+        std::string getName () const {     // Adding const to getter methods indicates that these methods do not modify the state of the object
+            return name;
+        }
+
+        int getID () const {
+            return ID;
+        }
+
+        void getGrades (float outGrades[3]) const {
+            for (int i=0; i<3; i++) {
+                outGrades[i] = grades[i];
+            }
+        }
+
+        // Function to calculate the average grade
+        float CalculateAverageGrade () {
+            float sum = 0.0f;    // 0.0f explicitly indicates a floating-point literal of type float. The f suffix specifies that the literal is a float
+            for (int i=0; i<3; i++) {
+                sum += grades [i];
+              }
+            return sum;
+        }
+
+
+
+
+};
---
+++ student grades management system/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "classes.hpp"
+
+using namespace std;
+
+int main() {
+
+
+}
